db8ff56107977611c750503f071fb146978235a0
Christian Walde <walde.christian@googlemail.com>
2012-01-12 13:46:29 +0100

    don't save the key count of $free and only retrieve it when actually needed

--- meteor.pl
+++ meteor.pl
@@ -179,10 +179,9 @@ sub solve {
         my $fp_cands = $fp_i_cands->[$p];
         for my $fpa (@{$fp_cands}) {
 
-            $nb = scalar keys %{$free};
             $nc = scalar grep { exists $free->{$_} } @{$fpa};
 
-            next if @{$fpa} != $nc and (@{$fpa} != $nc || $nb != $nc);
+            next if @{$fpa} != $nc and (@{$fpa} != $nc || keys(%{$free}) != $nc);
 
             for my $ci (@{$fpa}) {
                 $curr_board[$ci] = $p;
